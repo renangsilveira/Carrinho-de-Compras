9b0afd705ff06c9ae46febd8d1e5c4b52ef378e9
30935490 <30935490@090028-P14S13.macklabs.br>
2012-09-20 22:55:22 -0300

    Criando Preco Total FINAL

--- Carrinho/test/CarrinhoTeste.java
+++ Carrinho/test/CarrinhoTeste.java
@@ -51,6 +51,22 @@ public class CarrinhoTeste {
         c.removeProduto("Nevermind");
     
     }
+    
+    @Test
+    public void TestCalculaTot(){
+        
+        Carrinho c = new Carrinho();
+        Produto p1 = new Produto("Nevermind", 50.0, 1, 5);
+        Produto p2 = new Produto("Animals", 33, 2, 3);
+        Produto p3 = new Produto("Dark Side of the Moon", 78.5, 3, 12);
+        
+        c.adicionaProduto(p1);
+        c.adicionaProduto(p2);
+        c.adicionaProduto(p3);
+        
+        c.calculaPreco();
+        
+    }
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
